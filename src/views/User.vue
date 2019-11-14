fc1fe82f3bde75474033291b4e510fb862b3028e
Dimitri Casier <dimitri.casier@howest.be>
2019-11-14 11:15:15 +0100

    [TASK] - init commit

---
+++ src/views/User.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="user">
+    <template v-if="user !== false">
+      <article>
+        <div>
+          <h1>{{ user.name.title}} {{ user.name.first}} {{ user.name.last}}</h1>
+          <img :src="user.picture.large" />
+        </div>
+        <button @click="fetchUser">next!</button>
+      </article>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      url: "https://randomuser.me/api/",
+      user: false
+    };
+  },
+  created() {
+    this.fetchUser();
+  },
+  methods: {
+    fetchUser() {
+      fetch(this.url)
+        .then(response => {
+          return response.json();
+        })
+        .then(json => {
+          this.user = json.results[0];
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
